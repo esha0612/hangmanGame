9b4326569c7d249f277e79032d60ef3211b8282b
esha0612 <73080687+esha0612@users.noreply.github.com>
2024-05-13 12:00:15 -0700

    Create Countries.java

---
+++ src/Countries.java
@@ -0,0 +1,31 @@
+import java.net.URL;
+
+
+public class Countries extends WordCategory {
+
+    public Countries() {
+
+        super("Countries");
+        super.addWord("Brazil", "The capital of Brazil is Brasília");
+        super.addWord("Canada", "The capital of Canada is Ottawa");
+        super.addWord("Chile", "The capital of Chile is Santiago");
+        super.addWord("China", "The capital of China is Beijing");
+        super.addWord("France", "The capital of France is Paris");
+        super.addWord("Greece", "The capital of Greece is Athens");
+        super.addWord("India", "The capital of India is New Delhi");
+        super.addWord("Japan", "The capital of Japan is Tokyo");
+        super.addWord("Kenya", "The capital of Kenya is Nairobi");
+        super.addWord("Mexico", "The capital of Mexico is Mexico City");
+        super.addWord("Nigeria", "The capital of Nigeria is Abuja");
+        super.addWord("Russia", "The capital of Russia is Moscow");
+        super.addWord("Spain", "The capital of Spain is Madrid");
+        super.addWord("Sudan", "The capital of Sudan is Khartoum");
+        super.addWord("Turkey", "The capital of Turkey is Ankara");
+        super.addWord("Vietnam", "The capital of Vietnam is Hanoi");
+        super.addWord("Korea", "The capital of Korea is Seoul");
+
+
+
+    }
+
+}
