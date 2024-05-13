21c6a563955eb53f6d8d57634be35c3df0c95508
esha0612 <73080687+esha0612@users.noreply.github.com>
2024-05-13 12:03:45 -0700

    Create Planets.java

---
+++ src/Planets.java
@@ -0,0 +1,18 @@
+import java.net.URL;
+
+public class Planets extends WordCategory {
+
+    public Planets() {
+        super("Planets");
+        super.addWord("Mercury", "Closest to the Sun, smallest planet.");
+        super.addWord("Venus", "Hottest planet, thick carbon dioxide atmosphere.");
+        super.addWord("Earth", "Supports life, our home.");
+        super.addWord("Mars", "Red planet, iron oxide surface.");
+        super.addWord("Jupiter", "Largest planet, Great Red Spot storm.");
+        super.addWord("Saturn", "Rings made of ice and dust.");
+        super.addWord("Uranus", "Tilted sideways, unique rotation.");
+        super.addWord("Neptune", "Farthest planet, discovered mathematically.");
+        
+    }
+    
+}
