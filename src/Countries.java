7090fd04141792d13fc3603b7d520fdedcb55eb2
esha0612 <73080687+esha0612@users.noreply.github.com>
2024-05-12 22:25:07 -0700

    Create Countries.java

---
+++ src/Countries.java
@@ -0,0 +1,18 @@
+public class Countries extends WordCategory {
+
+    public Countries() {
+        super("Countries");
+        super.addWord("United States","USA");
+        super.addWord("Italy","Capital: Rome");
+        super.addWord("Germany","Capital: Berlin");
+        super.addWord("Canada","Capital: Ottawa");
+        super.addWord("Greece","Country in Europe");
+        super.addWord("India","Capital: New Dehli");
+        super.addWord("Mexico","Capital: Mexico City");
+        super.addWord("Japan","Capital: Tokyo"); 
+        super.addWord("Russia","Capital: Moscow");
+        super.addWord("Brazil","Capital: Brasília");
+        super.addWord("China","Capital: Beijing");
+    }
+
+}
