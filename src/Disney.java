23a63f4343d6638bf30709f5b6c5f1186ad30cdc
esha0612 <73080687+esha0612@users.noreply.github.com>
2024-05-13 12:01:45 -0700

    Create Disney.java

---
+++ src/Disney.java
@@ -0,0 +1,33 @@
+import java.net.URL;
+
+
+public class Disney extends WordCategory {
+
+    public Disney() {
+
+        super("Disney");
+        super.addWord("Moana", "Features Disney's first Polynesian princess.");
+        super.addWord("Tangled", "Rapunzel's hair has 100,000 CGI strands.");
+        super.addWord("Frozen", "\"Let It Go\" won an Oscar.");
+        super.addWord("Aladdin", "Robin Williams improvised as the Genie.");
+        super.addWord("Mulan", "Mulan isn't royal.");
+        super.addWord("Brave", "Merida has no love interest.");
+        super.addWord("Cinderella", "Released in 1950, a classic fairy tale.");
+        super.addWord("Hercules", "Greek pottery inspired the animation.");
+        super.addWord("Tarzan", "First Disney DVD/VHS simultaneous release.");
+        super.addWord("Zootopia", "City with 12 climate-based districts.");
+        super.addWord("Coco", "Won the 2018 Best Animated Feature Oscar.");
+        super.addWord("Onward", "Features Pixar's first openly LGBTQ+ character.");
+        super.addWord("Ratatouille", "Filmmakers researched Paris for authenticity.");
+        super.addWord("Up", "Opened the Cannes Film Festival.");
+        super.addWord("Cars", "Paul Newman's last film.");
+        super.addWord("Inside Out", "Emotions' designs reflect their feelings.");
+        super.addWord("Brave", "Originally titled \"The Bear and the Bow\".");
+        super.addWord("Finding Nemo", "Dory's popularity led to a sequel.");
+        super.addWord("Dumbo", "Released in 1941, one of Disney's shortest films.");
+
+
+
+    }
+
+}
