7ff59a6a7e669426b52d7a467f0afd6531e23ec7
esha0612 <73080687+esha0612@users.noreply.github.com>
2024-05-13 10:10:19 -0700

    Create JavaTerms.java

---
+++ src/JavaTerms.java
@@ -0,0 +1,34 @@
+public class JavaTerms extends WordCategory {
+
+    public JavaTerms() {
+        super("JavaTerms");
+        super.addWord("Boolean","True or false");
+        super.addWord("Integer","Stores integers");
+        super.addWord("Double","Stores decimal values");
+        super.addWord("Recursion","Calling the method within a methid");
+        super.addWord("Conditional","If/else statements");
+        super.addWord("Signature","Combination of method's name and parameter list");
+        super.addWord("Private","Access modifier used for methods/constructors/attributes");
+        super.addWord("Object","Instance of a java class");
+        super.addWord("Void","Returns nothing");
+        super.addWord("Super","Keyword to call the parent class");
+        super.addWord("Extends","Keyword for inheritance");
+        super.addWord("String","Data type storing text");
+        super.addWord("Class","A class is a blueprint or template for creating objects. public _____");
+        super.addWord("Array","Used to store many values in a single variable");
+        super.addWord("Random","Random class used to generate random values");
+        super.addWord("Binary","A searching algorithm that is efficinent and used on arrays that are sorted");
+        super.addWord("Linear","A searching algorithm that sequentially searches for an  element in the list");
+        super.addWord("Merge","Sorting algorithm that uses the divide and conquer approach. Time complexity of Merge Sort is O\(n*Log \n)");
+        super.addWord("Static","Static is a keyword used to declare members (variables and methods) that belong to the class");
+        super.addWord("Arraylist","Provides dynamic arrays that can grow as needed");
+        super.addWord("Selection","Sorting algorthm that works by finding the smallest element in the list and swaps with first element of list");
+        super.addWord("Insertion","Sorting algorithm that builds a sorted element one element at a time without swapping");
+        super.addWord("Bubble","Sorting algorithm which repeatedly steps through the list to be sorted, compares adjacent elements, and swaps them if they are in the wrong order");
+        super.addWord("Polymorphism","Polymorphism is a feature that allows objects of different classes to be treated as objects of a common superclass");
+        super.addWord("Final","Final is a keyword used to declare that a variable, method, or class cannot be changed or overridden");
+        super.addWord("Debug","Debugging refers to the process of identifying and fixing errors or bugs in a program");
+
+    }
+
+}
